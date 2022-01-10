6a5db94e2b88fe15d82a359b2761a97f1251ac04
佐藤　祐弥 <y.satou@satouyuuwatarunoMacBook-Air.local>
2022-01-10 22:12:52 +0900

    start_lesson6

---
+++ python.py
@@ -0,0 +1,13 @@
+# 2次元リスト
+player = "忍者"
+team =[player ,"勇者","戦士","魔法使い"]
+
+print(team)
+
+team_b = [team[0],team[1],team[2],team[3]]
+team_c = ["ドラゴン","スライム","魔王","うんち"]
+teams = [team_b, team_c] #二次元リスト
+print(team_b)
+print(teams)
+print(teams[1])
+print(teams[1][2])
